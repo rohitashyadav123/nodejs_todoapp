dfec3914b59ffc80cdca3eb729051ec7ac843f67
Pawan <mryadav68984@gmail.com>
2023-08-18 22:10:38 +0530

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+import express  from "express";
+import { register, login, getMyProfile,logout,} from "../controllers/user.js";
+import isAuthenticated from "../middleware/auth.js";
+
+const router=express.Router();
+
+
+
+router.post("/new", register);
+router.post("/login",login);
+router.get("/logout",logout);
+router.get("/me",isAuthenticated,getMyProfile);
+
+
+
+
+export default router;
